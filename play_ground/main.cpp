a8be91208b1190d1abd227f7416fcdf6520dc0ad
Kippei Watanabe <k6play7210@gmail.com>
2020-08-27 17:11:36 +0900

    Initial Commit

---
+++ play_ground/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  play_ground
+//
+//  Created by 渡部吉平 on 2020/08/27.
+//  Copyright © 2020 banana. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
