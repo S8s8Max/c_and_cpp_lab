7ae22a902a11140028ccdd7735781a31fad1c904
Kippei Watanabe <k6play7210@gmail.com>
2020-09-19 18:53:26 +0900

    first commit

--- play_ground/main.cpp
+++ play_ground/main.cpp
@@ -1,15 +1,19 @@
-//
-//  main.cpp
-//  play_ground
-//
-//  Created by 渡部吉平 on 2020/08/27.
-//  Copyright © 2020 banana. All rights reserved.
-//
+#include "all.h"
 
-#include <iostream>
+auto output_all = [](auto first, auto last)
+{
+    for (auto iter = first; iter != last; iter++)
+    {
+        cout << *iter << " ";
+    }
+    cout << endl;
+};
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    //istream_iterator<int> first(cin), last;
+    //output_all(first, last);
+
+    cout << endl;
     return 0;
 }
